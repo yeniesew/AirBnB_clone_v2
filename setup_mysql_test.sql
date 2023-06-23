ecffdb271b7da59fafb02ea5c650e85e7b081fdb
Biny19 <binalfe.tadele@gmail.com>
2023-06-23 05:32:23 -0700

    team project

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,5 @@
---creates a database a new user with specified permissions
+-- Prepares a MySQL Test server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-FLUSH PRIVILEGES;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
