809bf517e9c7020c378e6aa56ee70a28112d0b02
Biny19 <binalfew.tadele@gmail.com>
2023-07-06 11:10:47 -0700

    AirBnB clone v2 deploy static project

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-from fabric.api import local
+"""Fabric script to generates a .tgz"""
+
+
+from fabric.api import *
+import os.path
+from os import path
 from datetime import datetime
+from os.path import exists
 
 
-from fabric.decorators import runs_once
-
-
-@runs_once
 def do_pack():
-    """ Write a script that generates archive the contents of web_static folder"""
-    local("mkdir -p versions")
-    path = ("versions/web_static_{}.tgz"
-            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
-    result = local("tar -cvzf {} web_static"
-                   .format(path))
-
-    if result.failed:
-        return None
-    return path
+    """ creates tar archive"""
+    if path.exists("versions") is False:
+        local("mkdir versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    pathfile = "versions/web_static" + date + ".tgz"
+    local('tar cvfz ' + pathfile + ' web_static')
+    if exists(pathfile):
+        return (pathfile)
+    return None
